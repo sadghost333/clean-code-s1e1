861362d93c902f3b3a93285083aa322bb5f731f9
«Oleksandr <sdghst333@gmail.com>
2022-10-28 19:00:32 +0300

    refactor: fis selectors name in css and html first

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new__task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("in-complete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
